003ac209fef6b5c2e468d4283fe790c462042ea0
kushalkumarcs99 <37450254+kushalkumarcs99@users.noreply.github.com>
2020-06-30 08:06:55 +0530

    Create Angry Professor.cpp

---
+++ Angry Professor.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        int a[n+1];
+        for(int i=0;i<n;i++)
+        cin>>a[i];
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]<=0)
+            count++;
+        }
+        if(count>=k)
+        cout<<"NO"<<endl;
+        else
+        cout<<"YES"<<endl;
+    }
+    return 0;
+}
