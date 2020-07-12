2be23139a102de319cd56bcdb93801e3213c1ff6
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2020-07-12 12:07:17 +0530

    Create Sum vs XOR.cpp

---
+++ Sum vs XOR.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    unsigned long long n; std::cin >> n;
+
+    unsigned long long bin_zeros = 0;
+    for (unsigned long long i = 1; i > 0 && i < n; i = i << 1)
+        bin_zeros += !(n & i);
+
+    std::cout << (1L << bin_zeros) << std::endl;
+    return 0;
+    
+}
